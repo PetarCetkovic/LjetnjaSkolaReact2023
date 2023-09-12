d880e6c32184ac7dda1a828d250027207eb57d5c
Petar Ćetković <39220769+PetarCetkovic@users.noreply.github.com>
2023-09-12 08:51:09 +0000

    useEffect and pagination example

--- my-react-app/src/components/RickAndMortyCharacters.jsx
+++ my-react-app/src/components/RickAndMortyCharacters.jsx
@@ -3,10 +3,13 @@ import { Card } from "./Card"
 
 export const RickAndMortyCharacters = () => {
     const [characters, setCharacters] = useState([])
+    const [page, setPage] = useState(1)
+
+    const [disabled, setDisabled] = useState(true)
 
     const handleFetchCharacters = async () => {
         try {
-            const response = await fetch("https://rickandmortyapi.com/api/character")
+            const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
             const data = await response.json()
 
             setCharacters(data.results)
@@ -17,9 +20,23 @@ export const RickAndMortyCharacters = () => {
 
     useEffect(() => {
         handleFetchCharacters()
-    }, [])
+    }, [page])
+
+    useEffect(() => {
+        console.log("PAGE: ", page)
+        // handleFetchCharacters()
+    }, [page])
+
+    const handlePageChange = (currPage) => {
+        if(currPage>0) setPage(currPage)
+        setDisabled(currPage == 1)
+    }
 
     return <>
+        <button disabled={disabled} onClick={()=>{handlePageChange(page-1)}}>PREV</button>
+        <button onClick={()=>{handlePageChange(page+1)}}>NEXT</button>
+
         {characters.map(({name,status,image,location, url}, index) => <Card key={index} name={name} status={status} image={image} location={location} url={url}/>)}
+
     </>
 }
