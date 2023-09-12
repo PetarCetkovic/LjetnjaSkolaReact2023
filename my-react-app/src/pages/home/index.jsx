24d9a3d073a9af9eb1787f6d75e3b70f5437a3d5
Petar Ćetković <39220769+PetarCetkovic@users.noreply.github.com>
2023-09-12 13:47:57 +0000

    Day 2 - axios service initialization

--- my-react-app/src/pages/home/index.jsx
+++ my-react-app/src/pages/home/index.jsx
@@ -1,3 +1,33 @@
+import { useState, useEffect } from 'react';
+import { rickAndMortyService } from '../../service/rick-and-morty';
+
+const { getAllCharacters } = rickAndMortyService;
+
 export const HomePage = () => {
-    return <h1>Home page</h1>
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const result = await getAllCharacters()
+console.log(result);
+            setData(result.data);
+        } catch (err) {
+            setError(err);
+        }
+    };
+
+    fetchData();
+
+    }, []);
+
+    if (error)
+        return <div>Error: {error.message}</div>;
+
+
+    if (!data)
+        return <div>Loading...</div>;
+
+    return <div>Data: {JSON.stringify(data)}</div>;
 }
